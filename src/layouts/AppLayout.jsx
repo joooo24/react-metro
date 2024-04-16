d0baeaebe641126686593c3e4ea907aa202c16e0
suhyang1166 <yooyootea1166@gmail.com>
2024-04-17 00:28:29 +0900

    chore: header component 추가

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./AppLayout.css";
-import Header from "./Header";
+import Header from "./component/Header";
 
 const AppLayout = () => {
   return (
