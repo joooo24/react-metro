05cef919060b11715fd0e3d9ffe23a2c3ed56aba
claire <61533589+joooo24@users.noreply.github.com>
2024-04-18 00:48:18 +0900

    chore: 변경된 api 변수 반영

--- src/hooks/useStationAddress.js
+++ src/hooks/useStationAddress.js
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { api_via } from "../utils/api";
+import { api_reqre_time } from "../utils/api";
 
 const fetchStationAddress = ({ startIdx, endIdx }) => {
-    return api_via.get(`StationAdresTelno/${startIdx}/${endIdx}`);
+    return api_reqre_time.get(`StationAdresTelno/${startIdx}/${endIdx}`);
 };
 
 // 서울교통공사 지하철역 주소 및 전화번호 정보
