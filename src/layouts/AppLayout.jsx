84fed188343ecf7cdf8fa50b2b97e591fc5c3dbc
ssuminii <tnalsdldkr2@naver.com>
2024-04-18 17:16:28 +0900

    feat: footer ui작업

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./AppLayout.css";
+import Footer from './component/Footer/Footer';
 
 const AppLayout = () => {
     return (
         <>
             <div>AppLayout</div>
             <Outlet />
+            <Footer />
         </>
     );
 };
--- src/layouts/component/Footer/Footer.jsx
+++ src/layouts/component/Footer/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Footer.css';
-
+import { FaGithub } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaFacebookSquare } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='footer'>
+        <div className='social-networks'>
+            <span>Get connected with us on social networks</span>
+            <div><FaGithub className='img'/> <IoLogoInstagram className='img'/> <FaFacebookSquare className='img'/> <IoLogoYoutube className='img'/> <FaTwitter className='img'/></div>
+        </div>
+        
+        <div className='links'>
+            <ul>
+                <li>Home</li>
+                <li>Information</li>
+                <li>Contact</li>
+                <li>Products</li>
+                <li>Our Team</li>
+            </ul>
+        </div>
+
+        <div className='copyright'>
+            © 2024 Copyright: 코딩알려주는누나 react-study-3조
+        </div>
+    </div>
+
+    );
+};
 
 export default Footer
