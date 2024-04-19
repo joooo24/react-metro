ff14cece4d44582641f9ff6f21e3b05fc46e77f4
Hyunjeong Joo <61533589+joooo24@users.noreply.github.com>
2024-04-19 13:01:29 +0900

    chore: 지하철역상세페이지 컴포넌트 분리

--- src/pages/StationDetailPage/StationDetailPage.jsx
+++ src/pages/StationDetailPage/StationDetailPage.jsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import StationAddressInfo from "./component/StationAddressInfo/StationAddressInfo";
 import ArrivalInfo from "./component/ArrivalInfo/ArrivalInfo";
 import RealTimeInfo from "./component/RealTimeInfo/RealTimeInfo";
+import StationList from "./component/StationList/StationList";
 import "./StationDetailPage.css";
 
 const StationDetailPage = () => {
@@ -14,16 +15,18 @@ const StationDetailPage = () => {
         <div className="station-detail-page">
             <div className="map-wrap">지도</div>
             <div className="station-information">
-                <div className="station-list">
-                    <div className="station-name">역 이름</div>
-                    <div className="station-name">{currentStation}</div>
-                </div>
+                {/* 역 리스트 */}
+                <StationList currentStation={currentStation} />
+
                 {/* 도착정보 */}
                 <ArrivalInfo currentStation={currentStation} />
+
                 <button className="btn-show-station">지하철 노선도 보기</button>
+
                 {/* 실시간 도착정보 */}
                 <RealTimeInfo currentStation={currentStation} />
-                {/* 지하철 정보 */}
+
+                {/* 역 주소 */}
                 <StationAddressInfo currentStation={currentStation} />
             </div>
         </div>
