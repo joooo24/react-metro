e5a492eda2b29394b07a3cdc161b3bcf248c827a
leesanghun <lsh93620@gmail.com>
2024-04-20 04:03:38 +0900

    수정중

--- src/pages/StationDetailPage/component/StationList/StationList.jsx
+++ src/pages/StationDetailPage/component/StationList/StationList.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React,{useEffect} from "react";
+import { useRealtimePositionQuery } from "../../../../hooks/useRealtimePosition";
+
 
 const StationList = ({ currentStation }) => {
+    console.log(currentStation); // 강남역
+    const { data: stationPosition, isLoading, isError, error } = useRealtimePositionQuery({
+        startIdx: 1,
+        endIdx: 50,
+        statnNm: currentStation // 현재 역을 subwayNm에 넣기
+    });
+    console.log(stationPosition?.statnNm);
+   
     return (
         <div className="station-list">
             <div className="station-name">역 이름</div>
