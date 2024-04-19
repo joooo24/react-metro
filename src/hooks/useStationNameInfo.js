c45925f11599b5ebfb7d880b4f42fa1f5e529c2e
claire <61533589+joooo24@users.noreply.github.com>
2024-04-19 22:36:14 +0900

    chore: 서울시 지하철역 정보 검색 (역명) 주석

--- src/hooks/useStationNameInfo.js
+++ src/hooks/useStationNameInfo.js
@@ -5,7 +5,7 @@ const fetchStationNameInfo= ({ startIdx, endIdx, lineNm }) => {
     return api_name_info.get(`SearchInfoBySubwayNameService/${startIdx}/${endIdx}`)
 }
 
-// 역간 거리 및 소요시간 정보
+// 서울시 지하철역 정보 검색 (역명)
 export const useStationNameInfoQuery = ({ startIdx, endIdx }) => {
     return useQuery({
         queryKey: ['station-name', { startIdx, endIdx }],
