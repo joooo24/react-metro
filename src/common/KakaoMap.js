4ac9bd81def6357b0d792c5f2c9368c866a6797f
장 재 훈 <peng2412@naver.com>
2024-04-19 22:07:28 +0900

    feat: 카카오지도, json-server-지하철역위경도 데이터

---
+++ src/common/KakaoMap.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+
+const KakaoMap = ({ statnLat, statnLng }) => {
+
+    return (
+        <div>
+            {statnLat ?
+                <Map Map
+                    center={{ lat: statnLat, lng: statnLng }}
+                    style={{
+                        width: '330px',
+                        height: '620px',
+                        borderRadius: '20px',
+                        border: '1px solid gray'
+                    }}
+                >
+                    <MapMarker
+                        style={{ border: 'tranparent' }}
+                        position={{ lat: statnLat, lng: statnLng }}
+                    />
+                </Map>
+                : <div>아직 찾지 못했습니다.</div>}
+        </div >
+
+    );
+};
+
+export default KakaoMap
