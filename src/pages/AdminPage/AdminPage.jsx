baf00fa9dc8854ce82ffae2f74cf389078496d9e
장 재 훈 <peng2412@naver.com>
2024-04-20 19:14:52 +0900

    feat: adminPage, 패스키에 따른 URL 다이렉션

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import './AdminPage.style.css'
 import { useNavigate } from 'react-router-dom';
 
 
 const AdminPage = () => {
     const navigate = useNavigate()
+    const [adminAuth, setAdminAuth] = useState(false)
 
-    const passKey = prompt("관리자 PassKey를 입력하세요.");
-    if (passKey == '1234') {
-        alert(`안녕하세요, 관리자님!`);
-    } else {
-        alert("PassKey가 일치하지 않습니다.");
-        navigate('/')
-    }
+    useEffect(() => {
+        const passKey = prompt("관리자 PassKey를 입력하세요.");
+        if (passKey === '1234') {
+            alert(`안녕하세요, 관리자님!`);
+            setAdminAuth(true)
+        } else {
+            alert("PassKey가 일치하지 않습니다.");
+            navigate('/');
+        }
+    }, [navigate]);
 
     return (
         <div className='admin-page'>
-            여기는 관리자 페이지입니다!
+            {adminAuth &&
+                <div>여기는 관리자 페이지입니다.</div>
+            }
         </div>
     )
 }
