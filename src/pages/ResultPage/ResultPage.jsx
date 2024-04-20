8fb3d17ddbce050ddccb8ac231f74c4280ab46a0
장 재 훈 <peng2412@naver.com>
2024-04-20 21:25:26 +0900

    feat: 1회환승, 경로탐색 로직 구현

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -9,11 +9,18 @@ import { useStationPositionQuery } from "../../hooks/useStationPosition";
 import { useRealtimePositionQuery } from '../../hooks/useRealtimePosition'
 import { calTime } from "../../utils/calTime";
 import ReportForm from "./component/ReportForm/ReportForm";
+import { useStationReqreTimeQuery } from "../../hooks/useStationReqreTime";
 
 const startIdx = 0
 const endIdx = 50
 
 const ResultPage = () => {
+
+    let departToStopList = []
+    let stopToArriveList = []
+    let stopList = []
+    let stopStatn
+
     const [query, setQuery] = useSearchParams()
     const departStatnNm = query.get("depart").replace(/역$/, '');
     const arriveStatnNm = query.get("arrive").replace(/역$/, '');
@@ -29,8 +36,57 @@ const ResultPage = () => {
     //실시간 도착 
     const { data: ArrivalList, isLoading, isError, error } =
         useRealtimePositionQuery({ startIdx, endIdx, statnNm: departStatnNm })
+    //역간 거리정보 (출발호선, 도착호선)
+    const { data: departLineStatnList } = useStationReqreTimeQuery({ startIdx, endIdx, lineNm: departLine })
+    const { data: arriveLineStatnList } = useStationReqreTimeQuery({ startIdx, endIdx, lineNm: arriveLine })
+    console.log("ddd", departLineStatnList)
+    console.log("aaa", arriveLineStatnList)
 
-    console.log("실시간 도착", ArrivalList)
+    if (departLine != arriveLine && statnPosDB) {
+        const departStopList = departLineStatnList?.map(station => station.STATN_NM)
+        console.log("departStopList", departStopList)
+        let ResultStatn
+        for (let i = 0; i < departStopList?.length; i++) {
+            let findStatnName = departStopList[i]
+            let findLineName = arriveLine
+            ResultStatn = statnPosDB.find((station) =>
+                station.StatnNm == findStatnName && station.LineNm == findLineName)
+            if (ResultStatn) {
+                console.log("찾았다", ResultStatn)
+                break;
+            }
+        }
+        stopStatn = ResultStatn
+        console.log("환승역", stopStatn)
+        //인덱스 찾기 -> slice()로 범위 추출하기
+        console.log(departStatnNm, stopStatn.StatnNm)
+        let startIndex = departLineStatnList?.findIndex(station => station.STATN_NM.includes(departStatnNm))
+        let stopIndex = departLineStatnList?.findIndex(station => station.STATN_NM == stopStatn.StatnNm)
+        console.log("출발, 정차", startIndex, stopIndex)
+        if (startIndex >= 0 && stopIndex >= 0 && startIndex <= stopIndex) {
+            departToStopList = departLineStatnList?.slice(startIndex, stopIndex + 1)
+        }
+        else if (startIndex >= 0 && stopIndex >= 0 && startIndex > stopIndex) {
+            [startIndex, stopIndex] = [stopIndex, startIndex]
+            departToStopList = departLineStatnList?.slice(startIndex, stopIndex + 1).reverse()
+        }
+
+        let stopIndex2 = arriveLineStatnList?.findIndex(station => station.STATN_NM == stopStatn.StatnNm)
+        let arriveIndex = arriveLineStatnList?.findIndex(station => station.STATN_NM.includes(arriveStatnNm))
+        console.log("정차2, 도착", stopIndex2, arriveIndex)
+        if (stopIndex2 >= 0 && arriveIndex >= 0 && stopIndex2 <= arriveIndex) {
+            stopToArriveList = arriveLineStatnList?.slice(stopIndex2, arriveIndex)
+        }
+        //index 값 조정
+        else if (stopIndex2 >= 0 && arriveIndex >= 0 && stopIndex2 > arriveIndex) {
+            [stopIndex2, arriveIndex] = [arriveIndex, stopIndex2]
+            stopToArriveList = arriveLineStatnList?.slice(stopIndex2, arriveIndex).reverse()
+        }
+        let allStopList = [...departToStopList, ...stopToArriveList]
+        console.log("departToStopList", allStopList)
+    }
+
+    // console.log("실시간 도착", ArrivalList)
 
     useEffect(() => {
         const statnPosition = statnPosDB?.find(station => station.StatnNm === departStatnNm)
@@ -42,8 +98,8 @@ const ResultPage = () => {
     }, [statnPosDB])
 
     const FindDepartTime = (ArrivalList) => {
-        let recptnDt = new Date(ArrivalList[2]?.recptnDt)
-        let barvlDt = parseInt(ArrivalList[2]?.barvlDt)
+        let recptnDt = new Date(ArrivalList[0]?.recptnDt)
+        let barvlDt = parseInt(ArrivalList[0]?.barvlDt)
         let result = calTime(recptnDt, barvlDt)
         console.log("출발시간 결과 : ", result)
         return result
