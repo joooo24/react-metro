2c07663d06b01d75a4be583089cb438cd2642393
claire <61533589+joooo24@users.noreply.github.com>
2024-04-20 22:57:00 +0900

    feat: 즐겨찾기 역 클릭 시 디테일페이지 이동

--- src/common/FavoriteStationList/FavoriteStationList.jsx
+++ src/common/FavoriteStationList/FavoriteStationList.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { MdClose } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromFavorites } from "../../store/favoritesSlice";
+import { useNavigate } from "react-router-dom";
 import "./FavoriteStationList.css";
 
 const FavoriteStationList = () => {
     const favorites = useSelector((state) => state.favorites.favorites);
     const dispatch = useDispatch();
+    const navigate = useNavigate(); // useNavigate 훅 사용
 
     const handleRemoveFavorite = (stationName) => {
         dispatch(removeFromFavorites(stationName));
+    };
+
+    const handleStationClick = (stationName) => {
+        navigate(`/station-detail?q=${encodeURIComponent(stationName)}`);
     };
 
     return (
@@ -20,7 +26,9 @@ const FavoriteStationList = () => {
             <ul className="header-bookmark-contents">
                 {favorites.map((stationName) => (
                     <li key={stationName}>
-                        <p>{stationName}</p>
+                        <p onClick={() => handleStationClick(stationName)}>
+                            {stationName}
+                        </p>
                         <p onClick={() => handleRemoveFavorite(stationName)}>
                             <MdClose />
                         </p>
