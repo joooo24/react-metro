3ffcaf0d953e1b072db0a0818c368462ece79fb5
claire <61533589+joooo24@users.noreply.github.com>
2024-04-21 15:33:42 +0900

    fix: api 원복

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,19 +2,19 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const api_real_time = axios.create({
-    baseURL: `https://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json`,
+    baseURL: `http://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json`,
 });
 
 export const api_reqre_time = axios.create({
-    baseURL: `https://openapi.seoul.go.kr:8088/${API_KEY}/json/`
+    baseURL: `http://openapi.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_full_time = axios.create({
-    baseURL: `https://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
+    baseURL: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_name_info = axios.create({
-    baseURL: `https://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
+    baseURL: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_map_position = axios.create({
