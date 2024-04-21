d6ff8bfb475a9d67b4c78b23f9d5201d3a23ef27
claire <61533589+joooo24@users.noreply.github.com>
2024-04-21 15:12:40 +0900

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const api_real_time = axios.create({
-    baseURL: `http://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json`,
+    baseURL: `https://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json`,
 });
 
 export const api_reqre_time = axios.create({
-    baseURL: `http://openapi.seoul.go.kr:8088/${API_KEY}/json/`
+    baseURL: `https://openapi.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_full_time = axios.create({
-    baseURL: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
+    baseURL: `https://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_name_info = axios.create({
-    baseURL: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
+    baseURL: `https://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_map_position = axios.create({
-    baseURL: `http://localhost:5000/stations/`
+    baseURL: `https://localhost:5000/stations/`
 })
