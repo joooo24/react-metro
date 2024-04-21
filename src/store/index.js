0d25f63670f080afd8360d8a46a524e80dafa59e
claire <61533589+joooo24@users.noreply.github.com>
2024-04-21 16:34:34 +0900

    fix: api

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
     key: "root",
     storage,
-    whitelist: ["reports"],
+    whitelist: ["reports", "favorites"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,19 +2,19 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const api_real_time = axios.create({
-    baseURL: `http://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json`,
+    REACT_APP_BACKEND_PROXY: `http://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json`,
 });
 
 export const api_reqre_time = axios.create({
-    baseURL: `http://openapi.seoul.go.kr:8088/${API_KEY}/json/`
+    REACT_APP_BACKEND_PROXY: `http://openapi.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_full_time = axios.create({
-    baseURL: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
+    REACT_APP_BACKEND_PROXY: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_name_info = axios.create({
-    baseURL: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
+    REACT_APP_BACKEND_PROXY: `http://openAPI.seoul.go.kr:8088/${API_KEY}/json/`
 })
 
 export const api_map_position = axios.create({
